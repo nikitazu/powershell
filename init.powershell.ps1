bcef767ac8b758a05b0d9ade48eec902c6a80165
Nikita B. Zuev <nikitazu@gmail.com>
2015-04-29 16:59:48 +0300

    Profile example with modules

--- init.powershell.ps1
+++ init.powershell.ps1
@@ -1,7 +1,5 @@
 (new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
 
-echo "import-module PsGet" >> $profile
-
 install-module PsUrl
 install-module Jump.Location
 Install-Module PSColor
---
+++ profile.ps1
@@ -0,0 +1,5 @@
+﻿import-module PsGet
+import-module PsUrl
+import-module Jump.Location
+import-Module PSColor
+import-Module PsHosts
