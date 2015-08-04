f653c09f7cc56d663dc7dff3ef654c850cd63b6a
Nikita Zuev <nikitazu@gmail.com>
2015-08-04 12:24:04 +0300

    pkill command
    pkill-vsdev command to kill visual studio dev servers

--- profile.ps1
+++ profile.ps1
@@ -3,3 +3,18 @@ import-module PsUrl
 import-module Jump.Location
 import-Module PSColor
 import-Module PsHosts
+
+function pkill {
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory=$True, ValueFromPipeline=$True, ValueFromPipelineByPropertyName=$True)]
+    [string]
+    $name
+  )
+
+  foreach($p in Get-Process -Name $name) { kill $p.Id }
+}
+
+function pkill-vsdev {
+  pkill WebDev.WebServer40
+}
